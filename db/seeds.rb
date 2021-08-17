4188fbbda385559f8c2607d84113d24d1220f45d
Nalicyclica <77972881+Nalicyclica@users.noreply.github.com>
2021-08-17 20:14:23 +0900

    Revert "音楽表示機能の実装"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-30.times do |i|
-  music = Music.create( category_id: i%3, genre_id: i%10, user_id: 1)
-  music.music.attach(io: File.open('public/musics/ゴブリンのテーマ.mp3'), filename: 'Gobbrin.mp3', content_type: 'music/mp3')
-  music.save
-end
