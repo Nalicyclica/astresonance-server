c68ceb8df421f31d3113522183b3bfd99bc03e7a
Nalicyclica <siruri0xf3@gmail.com>
2021-08-18 11:08:10 +0900

    音楽削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :musics, only: [:index, :create, :show] do
+  resources :musics, only: [:index, :create, :show, :destroy] do
     resources :titles, only: [:create]
   end
   resources :titles, only: [:show] do
