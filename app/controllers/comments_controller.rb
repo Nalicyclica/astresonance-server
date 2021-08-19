30da0dac5f78bdf0c69c50d57a1866c5a1ce4317
Nalicyclica <siruri0xf3@gmail.com>
2021-08-19 14:24:52 +0900

    ログイン状態、ユーザーIDによる情報取得制限の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_comment_by_id, only: [:destroy]
+  before_action :is_owner, only: [:destroy]
   def create
     comment = Comment.new(comment_params)
     if comment.save
@@ -9,11 +12,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    if comment.destroy
-      render json: comment
+    if @comment.destroy
+      render json: @comment
     else
-      render json: comment.errors
+      render json: @comment.errors
     end
   end
 
@@ -21,6 +23,17 @@ class CommentsController < ApplicationController
 
   def comment_params
     params_key = [:text]
-    params.require(:comment).permit(params_key).merge(user_id: params[:user_id], title_id: params[:title_id])
+    params.require(:comment).permit(params_key).merge(user_id: current_user.id, title_id: params[:title_id])
+  end
+
+  def find_comment_by_id
+    @comment = Comment.find(params[:id])
+  end
+
+  def is_owner
+    unless current_user.id == @comment.user_id
+      @comment.errors.add(:title, ' is Not owned')
+      render status: 400, json: @comment.errors
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
-    user = User.find(params[:id])
-    titles = Title.where(user_id: user.id).as_json(include: :music)
-    comments = Comment.where(user_id: user.id).as_json(include: :title)
-    musics = Music.where(user_id: user.id).joins(:titles).select('musics.*',
-                                                                 'titles.title').where(titles: { user_id: params[:user_id] })
-    render json: user.as_json.merge(titles: titles, comments: comments, musics: musics)
+    owner = User.find(params[:id])
+    titles = Title.where(user_id: owner.id).as_json(include: :music)
+    comments = Comment.where(user_id: owner.id).as_json(include: :title)
+    musics = Music.where(user_id: owner.id).joins(:titles).select('musics.*',
+                                                                  'titles.title').where(titles: { user_id: current_user.id })
+    render json: owner.as_json.merge(titles: titles, comments: comments, musics: musics)
   end
 end
