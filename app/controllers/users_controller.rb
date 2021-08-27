260e12d98f9bd68dd5e6ee38f112accab71e00e9
Nalicyclica <siruri0xf3@gmail.com>
2021-08-27 11:04:15 +0900

    ユーザー個別ページに送る音楽情報を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
     owner = User.select('users.id', 'users.nickname', 'users.icon_color', 'users.introduce').find(params[:id])
     titles = Title.where(user_id: owner.id).as_json
     comments = Comment.where(user_id: owner.id).as_json(include: :title)
-    musics = Music.where(user_id: owner.id).joins(:titles).select('musics.*',
-                                                                  'titles.title').where(titles: { user_id: current_user.id })
+    user_titles = Title.where(user_id: current_user.id).select('titles.title', 'titles.color', 'titles.music_id')
+    musics = Music.joins("LEFT OUTER JOIN (#{user_titles.to_sql}) user_titles ON musics.id = user_titles.music_id").select('musics.*','user_titles.title', 'user_titles.color').where(user_id: owner.id)
     render json: owner.as_json.merge(titles: titles, comments: comments, musics: musics)
   end
 end
